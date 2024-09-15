4dbf5c3de0b8d881169170d6f8ef4e6b5df45769
Mansi Verma <99127354+Vmansi115@users.noreply.github.com>
2024-09-15 13:08:35 +0530

    Add files via upload

--- src/config/Gemini.js
+++ src/config/Gemini.js
@@ -1,1 +1,40 @@
+/*
+ * Install the Generative AI SDK
+ *
+ * $ npm install @google/generative-ai
+ */
 
+import {
+  GoogleGenerativeAI,
+} from "@google/generative-ai";
+
+const apiKey = "AIzaSyA_blpIkgdiRsKYfQp01Cwbhk8Tei2up7g";
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 64,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function runchat(prompt) {
+  const chatSession = model.startChat({
+    generationConfig,
+ // safetySettings: Adjust safety settings
+ // See https://ai.google.dev/gemini-api/docs/safety-settings
+    history: [
+    ],
+  });
+
+  const result = await chatSession.sendMessage(prompt);
+  console.log(result.response.text());
+  return result.response.text();
+}
+
+export default runchat;
